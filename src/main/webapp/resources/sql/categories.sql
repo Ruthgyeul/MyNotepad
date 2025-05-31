03ad92d20ebfc9c33666572320f65b7ccfa41f6f
Ruthgyeul <wingd4705@gmail.com>
2025-05-31 22:14:46 +0900

    Update: update category sql and add initial sql

--- src/main/webapp/resources/sql/categories.sql
+++ src/main/webapp/resources/sql/categories.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS categories (
     id INT(11) NOT NULL AUTO_INCREMENT,
     user_id INT(11) NOT NULL,
     name VARCHAR(100) NOT NULL,
+    is_default BOOLEAN NOT NULL DEFAULT FALSE,
     created_at TIMESTAMP NULL DEFAULT current_timestamp(),
     updated_at TIMESTAMP NULL DEFAULT NULL ON UPDATE current_timestamp(),
     PRIMARY KEY (id),
@@ -15,8 +16,14 @@ CREATE TRIGGER IF NOT EXISTS after_user_insert
 AFTER INSERT ON users
 FOR EACH ROW
 BEGIN
-    INSERT INTO categories (user_id, name) VALUES
-        (NEW.id, 'general'),
-        (NEW.id, 'etc');
+    INSERT INTO categories (user_id, name, is_default) VALUES
+        (NEW.id, '기본', TRUE);
 END//
 DELIMITER ;
+
+-- 기존 사용자들을 위한 '기본' 카테고리 생성
+INSERT INTO categories (user_id, name, is_default)
+SELECT u.id, '기본', TRUE
+FROM users u
+LEFT JOIN categories c ON u.id = c.user_id AND c.name = '기본'
+WHERE c.id IS NULL;
