6bac4481d1b842470a4c843798eba377f8039bf3
Ruthgyeul <wingd4705@gmail.com>
2025-05-31 20:25:21 +0900

    Ref: remove java related files

--- src/main/resources/sql/categories.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE categories (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE,
-    label VARCHAR(50) NOT NULL
-);
-
-INSERT INTO categories (name, label) VALUES
-     ('general', '일반'),
-     ('etc', '기타');
--- src/main/resources/sql/noteFiles.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE noteFiles (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        note_id INT NOT NULL,
-        file_name VARCHAR(255),
-        file_data LONGBLOB,
-        uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (note_id) REFERENCES notes(id) ON DELETE CASCADE
-);
